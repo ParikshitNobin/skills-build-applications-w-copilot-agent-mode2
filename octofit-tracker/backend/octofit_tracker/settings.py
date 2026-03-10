e0af12bc763397fbe25852e261bbd46992b757c3
ParikshitNobin <parikshitsingh.nobin@accenture.com>
2026-03-10 11:19:10 +0000

    Setup Codespace Django URLs, ALLOWED_HOSTS, and API root endpoint for Codespace and localhost

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-_n1_9%lt3!kv%!gqifm$u*-g*q+h7zmgjeff=xs&_m82i3w&a+
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+import os
+# Allow localhost and Codespace public URL
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
